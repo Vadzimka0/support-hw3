a3eb73de70620851e0b87eb7ddb3750d917e943c
Vadzim Dzianisik <wadik.nhl@gmail.com>
2021-10-31 13:30:19 +0300

    refactoring: hw7 solution without styles

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -3,28 +3,36 @@ import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+  options?: any[]
+  onChangeOption?: (option: any) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = (
-    {
-        options,
-        onChange, onChangeOption,
-        ...restProps
-    }
+  {
+    options,
+    onChange, onChangeOption,
+    ...restProps
+  }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+  const mappedOptions: any[] = options ? options.map((o, i) => (
+    <option key={i} value={o}>
+      {o}
+    </option>
+  )) : []
 
-    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
-    }
+  const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+    onChange && onChange(e)
+    onChangeOption && onChangeOption(e.currentTarget.value)
+  }
 
-    return (
-        <select onChange={onChangeCallback} {...restProps}>
-            {mappedOptions}
-        </select>
-    )
+  return (
+    <select
+      onChange={onChangeCallback}
+      {...restProps}
+    >
+      {mappedOptions}
+    </select>
+  )
 }
 
 export default SuperSelect
