a5498b38ce1f6412b830c3723e89537e488056e8
Vadzim Dzianisik <wadik.nhl@gmail.com>
2021-11-06 21:29:03 +0300

    refactoring: hw8 after watch dz8 re

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,18 +1,16 @@
 import {UserType} from "../HW8";
 
-type SetPeopleActionType = {
-  type: 'sort' | 'check'
-  payload: 'up' | 'down' | number
-}
+type SetPeopleActionType = { type: 'sort', payload: 'up' | 'down' } | { type: 'check', payload: number }
 
 export const homeWorkReducer = (state: Array<UserType>, action: SetPeopleActionType): Array<UserType> => { // need to fix any
   switch (action.type) {
     case 'sort': {
-      let copyState;
-      (action.payload === 'up')
-        ? copyState = [...state].sort((a, b) => a.name > b.name ? 1 : -1)
-        : copyState = [...state].sort((a, b) => a.name < b.name ? 1 : -1);
-      return copyState
+      const copyState = [...state].sort( (a,b) => {
+        if (a.name > b.name) {return 1;}
+        if (a.name < b.name) {return -1;}
+        return 0;
+      })
+      return action.payload === 'up' ? copyState : copyState.reverse()
     }
     case 'check': {
       return state.filter(p => p.age >= action.payload)
